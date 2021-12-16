248ef5ee3bee89ec7e0edcf9bc7a2ecfa43542a6
Fraser Cook <fraserc6@gmail.com>
2021-12-16 12:10:35 +0000

    Added NHSX to page title
    TODO: confirm title/meta tags required

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function MyApp({ Component, pageProps }) {
    return (
       <>
          <Head>
-            <title>AI Dictionary</title>
+            <title>AI Dictionary | NHSX</title>
             <link rel="icon" href="/favicon.ico" />
             <link
                href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap"
