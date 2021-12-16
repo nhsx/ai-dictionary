36bfb3ad6044e03f54b6aa20cb845548bf170104
fraserc6 <fraserc6@gmail.com>
2021-12-16 12:17:45 +0000

    Deploying to gh-pages from @ nhsx/ai-dictionary@bfedd5046d75654014827ea16f50588f097dcefb 🚀

---
+++ _next/static/Ik1AG7-r1mcD_k7ZZMudu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/208-852c4227402bbaa1.js","static/chunks/pages/index-6a89a014c6aa087e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/fcDc9a3fGfkH8AwAnnWpD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/208-852c4227402bbaa1.js","static/chunks/pages/index-be2c84f7aefec5ad.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
