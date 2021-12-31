6da0293e7e8b27f528808b6643717ea58f7d3424
fraserc6 <fraserc6@gmail.com>
2021-12-31 15:51:25 +0000

    Deploying to gh-pages from @ nhsx/ai-dictionary@516a4e2558460081809ff78d6f3255e40f4586ba 🚀

--- _next/static/UzEYJPnJcgPnXFjOSat2A/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/772-ba0e43e6ec3a49c5.js","static/chunks/pages/index-9267e2f1ebb41df5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
