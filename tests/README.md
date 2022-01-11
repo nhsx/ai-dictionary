9fd80632fec5dec75d0aade0944ed5d5c5b5a668
Amadeus Stevenson <amadeus.stevenson@nhsx.nhs.uk>
2022-01-11 12:38:03 +0000

    Add json contents validation

---
+++ tests/README.md
@@ -0,0 +1,17 @@
+# Tests
+
+All tests are automatically run on every push to this repo.
+
+## Validate JSON schema
+
+The [JSON schema](../data/terms.schema.json) defines the required data structure of `terms.json`.
+
+This can be tested using: `jsonschema --instance terms.json terms.schema.json`
+
+## `validate_json.py`
+
+This python script extends json schema validation to ensure:
+
+1. `slug`: unique
+2. `title`: unique
+3. `related`: no references to non-existent slugs
---
+++ tests/validate_json.py
@@ -0,0 +1,60 @@
+# %% [markdown]
+# # Validate terms lists
+#
+# This python script extends json schema validation (`jsonschema --instance terms.json terms.schema.json`) to ensure:
+#
+# 1. slug: unique
+# 2. title: unique
+# 3. related: no references to non-existent slugs
+
+# %%
+# Import libraries
+import json
+
+
+# %%
+# Load terms
+try:
+    f = open("../data/terms.json")
+except:
+    raise Exception("Unable to open terms.json")
+
+try:
+    terms = json.load(f)
+except:
+    raise Exception("Unable to parse terms.json as valid json")
+
+# Check for unique slugs
+
+slugs = []
+for term in terms["terms"]:
+    if term["slug"] == "":
+        raise Exception(f"Blank slug for term \"{term['title']}\"")
+    if term["slug"] in slugs:
+        raise Exception(f"Duplicate slug found for \"{term['slug']}\"")
+    else:
+        slugs.append(term["slug"])
+
+print(f"{len(slugs)} unique slug(s) in database")
+
+# Check for unique titles
+
+titles = []
+for term in terms["terms"]:
+    if term["title"] == "":
+        raise Exception(f"Blank title for slug \"{term['slug']}\"")
+    if term["title"] in titles:
+        raise Exception(f"Duplicate title found for \"{term['title']}\"")
+    else:
+        titles.append(term["title"])
+
+print(f"{len(titles)} unique title(s) in database")
+
+# Check for orphaned slugs
+
+for term in terms["terms"]:
+    related = term["related"]
+    for related_slug in related:
+        if related_slug not in slugs:
+            raise Exception(f"Orphaned related slug found: {related_slug}")
+print("No orphaned slugs found")
