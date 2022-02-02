289527775148898862c04d4b9fbabbcc427e7265
amadeuspzs <amadeus.stevenson@gmail.com>
2022-02-02 13:27:19 +0000

    Deploying to gh-pages from @ nhsx/ai-dictionary@5225304c10b8e331f828743302596e3e8f761040 🚀

--- _next/static/LXmvIae1pjWXehjccl30s/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/772-569f210f758c3243.js","static/chunks/pages/index-b45b2f28e07f3230.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
