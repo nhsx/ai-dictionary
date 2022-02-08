05cd54409b03e936e470190a73d33aef6556c48a
amadeuspzs <amadeus.stevenson@gmail.com>
2022-02-08 10:29:17 +0000

    Deploying to gh-pages from @ nhsx/ai-dictionary@c243b6c323f320f9954d0f11a05ae0d7c6b22026 🚀

--- _next/static/Xe1W8Z_BLDMp9qq5QXPLZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/610-48930e9d4748c178.js","static/chunks/pages/index-657da002215d6ed1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/pRLIUXRxTAcbjTq1_xZbb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/610-48930e9d4748c178.js","static/chunks/pages/index-3a3a7361a256e081.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
