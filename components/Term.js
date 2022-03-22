b679c1251d356d7a59ad3dd3106390fe09b87eb7
Fraser Cook <fraserc6@gmail.com>
2022-03-22 13:49:21 +0000

    Add alternative name to term

--- components/Term.js
+++ components/Term.js
@@ -3,7 +3,7 @@ import { Dialog, Transition } from "@headlessui/react"
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/solid"
 import data from "data/terms.json"
 
-export default function Term({ isOpen, name, description, acronym, related, onClose, onNext, onPrev, onSelectTermSlug }) {
+export default function Term({ isOpen, name, description, acronym, alternateName, related, onClose, onNext, onPrev, onSelectTermSlug }) {
 
    /**
     * Get related terms from array
@@ -68,12 +68,19 @@ export default function Term({ isOpen, name, description, acronym, related, onCl
                      </div>
 
                      <Dialog.Description as="div" className="mt-6 text-xl md:text-2xl text-blue-200">
-                        {acronym && (
-                           <div className="inline-flex bg-black bg-opacity-20 px-3 py-1 rounded mb-8 text-sm space-x-2">
-                              <span>Also known as: </span>
-                              <span className="text-white">{acronym}</span>
-                           </div>
-                        )}
+                        <div className="flex space-x-2">
+                           {acronym && (
+                              <div className="inline-flex bg-black bg-opacity-20 px-3 py-1 rounded mb-8 text-sm space-x-2">
+                                 <span className="text-white">{acronym}</span>
+                              </div>
+                           )}
+                           {alternateName && (
+                              <div className="inline-flex bg-black bg-opacity-20 px-3 py-1 rounded mb-8 text-sm space-x-2">
+                                 <span>Also known as: </span>
+                                 <span className="text-white">{alternateName}</span>
+                              </div>
+                           )}
+                        </div>
                         <div id="term-content" dangerouslySetInnerHTML={{ __html: description }}></div>
                      </Dialog.Description>
 
